7ba57115d20073ad9bac4d83a8aa590d86142925
Mountdenyraj Nadar <mountdenyraj.n@thealchemygroup.co>
2021-07-19 14:58:04 +0530

    [Code changes] Adding new package for testing

--- sample.js
+++ sample.js
@@ -78,9 +78,13 @@
 // //   }
 // // });
 
-const player = new OpenPlayerJS('video', {
+const player = new OpenPlayerJS("video", {
+  mode: "responsive",
+  width: "740px",
   ads: {
-      src: 'https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/single_ad_samples&ciu_szs=300x250&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ct%3Dlinear&correlator='
+    debug: true,
+    src: "https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/single_ad_samples&ciu_szs=300x250&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ct%3Dlinear&correlator="
   },
 });
 player.init();
+player.play();
